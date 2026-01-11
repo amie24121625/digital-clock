c2decd718d3ed967ffb24f1a614708dd1f4ff808
amie24121625 <siddhikasri2006@gmail.com>
2026-01-11 14:39:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    function builtclock(){
+        const date=new Date();
+        let hours=date.getHours();
+        let minutes=date.getMinutes();
+        let seconds=date.getSeconds();
+        
+        hours=hours<10?"0"+hours:hours;
+        minutes=minutes<10?"0"+minutes:minutes;
+        seconds=seconds<10?"0"+seconds:seconds;
+
+        document.querySelector('#clock-hours').innerText=hours;
+        document.querySelector('#clock-min').innerText=minutes;
+        document.querySelector('#clock-sec').innerText=seconds;
+        setTimeout(builtclock,1000);
+    }builtclock();
+}
